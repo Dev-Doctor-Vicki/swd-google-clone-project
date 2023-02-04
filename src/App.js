1c9f05eec8fd1d54d9d05e44eeb94bdbe966adeb
Dr Vicki Bealman <drvicki.bealman@gmail.com>
2023-02-04 15:48:39 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,19 +1,13 @@
-
+import React from 'react';
 import './App.css';
+
+import Home from './components/Home';
 
 function App() {
   return (
-    <div className="App">
-      <header>
-        <h1>
-          Welcome to Final Project!
-        </h1>
-        <h3> Start coding with Dr. Vicki to create your own Google Clone!</h3>
-        <h5>FOLLOW INSTRUCTIONS VERY CAREFULLY</h5>
-        
-      </header>
+    <div className="app">
+      <Home />
     </div>
   );
 }
-
 export default App;
